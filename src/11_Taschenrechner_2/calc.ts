17b006ff083f4ebc70315ebfb92d7049979e821b
YvonneJL <yvonneloder@gmx.de>
2024-12-31 11:33:41 +0100

    alles ohne readme, beim Taschrechner Kommentare eingefügt

--- src/11_Taschenrechner_2/calc.ts
+++ src/11_Taschenrechner_2/calc.ts
@@ -1,18 +1,18 @@
 const ergebnis = document.getElementById("ergebnis");
 
 
-// & Funktion, um Buttons zu klicken und Rechnung anzeigen zu lassen
+// & Funktion, um Buttons klicken zu können (.addEventListener) und Rechnung anzeigen zu lassen (ergebnis.innerHTML += element.innerHTML)
 document.querySelectorAll(".numbers").forEach(element => { 
     console.log(element);
     // alle ausgewählten Elemente werden angezeigt
     // ! Was macht forEach?
-    // ! Und ist das mit dem fat arrow dann eine Funktion?
     element.addEventListener("click", () => {
         console.log(element);
         // hier bekommt man nur noch das Element angezeigt, auf das man gedrückt hat
         // ! .addEventListener
         if (ergebnis !==null) {
             ergebnis.innerHTML += element.innerHTML}
+            // ! Hier wird dem Ergebnisfeld jeder "ButtoN" hinzugefügt, der geklickt wurde, zB 2 + 1
             console.log(element.innerHTML);
         // ! if habe ich hinzugefügt, wegen TS-->sonst Fehlermeldung
     })
@@ -23,7 +23,7 @@ document.getElementById("result")?.addEventListener("click", () => {
     if (ergebnis !==null) {
         console.log(eval(ergebnis.innerHTML));
         ergebnis.innerHTML =(eval(ergebnis.innerHTML));
-        // ! eval zeigt Ergebnis an??
+        // ! eval zeigt dann Ergebnis von zuvor ins Ergebnisfeld eingebenen Zahlen und Operatoren
     }
 })
 
@@ -39,3 +39,17 @@ deleteCalc?.addEventListener("click", () => {
         //! zwei Anführungszeichen --> auf 0
     }
 })
+
+
+
+// ! .forEach () bearbeitet alle Elemente eines arrays
+// ! in meinem Fall, alle divs mit class=numbers
+// ! in die Klammern schreibt man dann die Funktion, die für alle Elemente ausgeführt werden soll
+
+
+// ! .addEventListener () überwacht ein Ereignis auf einem DOM Element und reagiert darauf
+// ! im Beispiel wird ein Klick-Ereignis hinzugefügt
+// ! in Klammern steht dann zuerste in Anführungszeichen das Event "click" und danach das Listener", die Funktion, die beim Event ausgelöst werden soll --> mit () => {funktion...}
+
+
+// ! eval () wertet String als TS Code aus und führt ihn aus
