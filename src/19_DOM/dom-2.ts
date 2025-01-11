8277339fd996e9645ef7dc27fc637b5a3ad3a092
YvonneJL <yvonneloder@gmx.de>
2025-01-11 09:37:41 +0100

    Bonusaufgabe DOM auch geschafft

--- src/19_DOM/dom-2.ts
+++ src/19_DOM/dom-2.ts
@@ -19,3 +19,48 @@ if (aTags && button) {
         aTagsAsArray[2].style.backgroundColor = "#396362";
     });
 };
+
+
+//& Dom-Elements-TS-Level-2_7
+console.log("Dom-Elements-TS-Level-2_7");
+
+//..Schreibe eine Funktion, die beim Anklicken des Buttons ein Element mit aufsteigenden Nummern erstellt.
+//..Nutze:
+    //..querySelector()
+    //..addEventListener()
+
+    //..let counter = 0;
+    //..createElement()
+    //..textContent()
+    //..appendChild()
+    //..classList
+
+    const buttonClick = document.querySelector("#click");
+    const divForElements = document.querySelector("#more-elements");
+
+    // global den Zähler definieren -->Warum?
+    let clickCount = 0;
+
+    if (buttonClick && divForElements) {
+        buttonClick.addEventListener("click", function() {
+            // als erstes den Zähler aufzählen
+            clickCount++
+            // dann mit if else, da das erste Div ganz anders im Vgl. zu allen anderen
+            // als Bedingung den Zähler
+            if (clickCount === 1) {
+                const divOne = document.createElement("div");
+                divOne.textContent = "0";
+                divOne.classList.add("weiss");
+                divForElements.appendChild(divOne);
+            // alle anderen Divs in der Bedingung in else zusammengefasst, da sie sich gleichen aber die Nummer aufgezählt wird
+            } else if (clickCount >=2 && clickCount <= 10) {
+                const divNext = document.createElement("div");
+                divNext.textContent = `${clickCount -1}`;
+                divNext.classList.add("rechteck");
+                divForElements.appendChild(divNext);
+            };
+        });
+    };
+
+    // ! Also keine Schleife in dem Sinn.
+    //! Es gibt aber trotzdem einen Zähler der hoch zählt
