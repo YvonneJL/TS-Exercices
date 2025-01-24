40c983ad57477eb1b3a997ca884f17461023ede5
YvonneJL <yvonneloder@gmx.de>
2025-01-24 13:35:14 +0100

    Async Aufgabe fertig

--- src/29_ASYNC_2/app.ts
+++ src/29_ASYNC_2/app.ts
@@ -4,11 +4,20 @@ import {waitForTwoSeconds} from "./patient-waiter.ts";
 //..Binde patient-waiter.ts ein
 //..Lege eine asynchrone Funktion mit dem Namen main an
 
-waitForTwoSeconds
+// waitForTwoSeconds();
 
-function main () {
+async function main () {
+    console.log("Hi, here I am...");
+    await waitForTwoSeconds();
+    console.log("Hello...");
+    console.log("Can you hear me?");
+};
 
-}
+
+// wenn ich die Funktion von oben OHNE AWAIT aufrufe, wird alles normal, also direkt geloggt
+// nur die waitForTwoSeconds wird erst nach 2 Sek ausgeführt, also als letztes, da alles andere schneller
+//async ändert in diesem Fall nichts.
+main();
 
 
 //..Schreibe in der Funktion auf die Konsole “Hi, here I am…”
@@ -17,3 +26,17 @@ function main () {
 //..Schreib danach auf die Konsole “Can you hear me…”
 //..Rufe main() auf
 //..Schau dir in der Ausgabe an, wann was geloggt wird
+
+
+
+
+//& async-TS-Level-1_2
+console.log("async-TS-Level-1_2");
+
+//..Jetzt soll auf die Ausführung von waitForTwoSeconds gewartet werden
+//..Nutze das Schlüsselwort await an der richtigen Stelle in der Funktion main
+//..Schau dir nun die Reihenfolge in der Ausgabe an
+
+
+//siehe oben:
+// mit "await" vor der Funktion (da diese Funktion abgewartet werden soll), wartet der Rest der Funktion (also erst unterhalb vom await) er und wird zusammen mit der Funktion waitForTwoSeconds ausgeführt
--- src/29_ASYNC_2/patient-waiter.ts
+++ src/29_ASYNC_2/patient-waiter.ts
@@ -7,8 +7,18 @@ console.log("async-TS-Level-1_1");
 //..Exportiere die Funktion
 //..Erstelle eine Datei app.ts
 
-export const waitForTwoSeconds: Promise<void> = new Promise ((resolve, reject)=> {
+// export const waitForTwoSeconds: Promise<void> = new Promise ((resolve, reject)=> {
+//     setTimeout(() => {
+//         console.log('Thank you for your patience');
+//     }, 2000);
+// });
+
+
+export function waitForTwoSeconds() {
+    return new Promise<void>((resolve) => {
     setTimeout(() => {
-        console.log('Thank you for your patience');
-    }, 2000);
-});
+    console.log("Thanx 4 the patience 💜");
+    resolve()
+    }, 2000); // Warte für 2 Sekunden
+    });
+    }
