3594db6c3604f7d32d2b33dc03c7c63db152a771
YvonneJL <yvonneloder@gmx.de>
2025-02-13 15:42:42 +0100

    all OOP 2 done including bonus

--- src/34_OOP/script.ts
+++ src/34_OOP/script.ts
@@ -12,10 +12,10 @@ import TVShow from "./Serie";
 
 //..Lege in einer neuen Datei eine Klasse mit dem Namen IceCreamFlavor an
 //..Die Klasse soll folgende Attribute haben
-    //..name: string
-    //..price: number
-    //..isPopular: boolean
-    //..description: string → optional
+//..name: string
+//..price: number
+//..isPopular: boolean
+//..description: string → optional
 //..name soll beim Anlegen ‘’ sein, price = 0, isPopular = false und description = undefined
 //..Lege 4 Instanzen von IceCreamFlavor an und setze deren Eigenschaften
 
@@ -23,13 +23,12 @@ const vanilla = new IceCreamFlavor();
 vanilla.name = "Vanilla";
 vanilla.price = 3;
 vanilla.isPopular = true;
-vanilla.description = "eine sehr langweilige Eissorte"
+vanilla.description = "eine sehr langweilige Eissorte";
 
 const yoghurt = new IceCreamFlavor();
 yoghurt.name = "Joghurt";
 yoghurt.price = 3.5;
 yoghurt.isPopular = true;
-
 
 const mangoNougat = new IceCreamFlavor();
 mangoNougat.name = "Mango Nougat";
@@ -40,50 +39,96 @@ const chocolate = new IceCreamFlavor();
 chocolate.name = "Schokolade";
 chocolate.price = 3;
 chocolate.isPopular = true;
-chocolate.description = "eine sehr langweilige Eissorte"
+chocolate.description = "eine sehr langweilige Eissorte";
 
 //..Lege die 4 Instanzen in einem Array ab
 
 const iceCreamFlavors = [];
 
-iceCreamFlavors.push(vanilla)
-iceCreamFlavors.push(yoghurt)
-iceCreamFlavors.push(mangoNougat)
-iceCreamFlavors.push(chocolate)
+iceCreamFlavors.push(vanilla);
+iceCreamFlavors.push(yoghurt);
+iceCreamFlavors.push(mangoNougat);
+iceCreamFlavors.push(chocolate);
 
 console.log(iceCreamFlavors);
 
 //..Lass dir von allen populären Eissorten (isPopular) den Namen auf der Konsole ausgeben
 
-iceCreamFlavors.forEach((element)=> {
-    element.showNameOfIcecream()
-})
-
-
-
+iceCreamFlavors.forEach((element) => {
+  element.showNameOfIcecream();
+});
 
 //& OOP-Class-TS-Level-1_12
 
 //..Wir bauen auf der Klasse IceCreamFlavors auf
 //..Die Klasse soll drei Methoden bekommen
-    //..getTotalPrice(numberOfScoops: number)
-    //..gibt den Gesamtpreis anhand der übergebenen Anzahl an Eiskugeln auf der Konsole aus
+//..getTotalPrice(numberOfScoops: number)
+//..gibt den Gesamtpreis anhand der übergebenen Anzahl an Eiskugeln auf der Konsole aus
 
-    console.log(mangoNougat.getTotalPrice(3));
+console.log(mangoNougat.getTotalPrice(3));
 
-    //..printInfo()
-    //..gibt z. B. Folgendes auf der Konsole aus
-    
-    vanilla.printInfo();
-    mangoNougat.printInfo();
-    
-    //..getLengthOfDescription()
-    //..gibt die Textlänge von description aus oder 0, wenn die Eigenschaft nicht gesetzt ist
+//..printInfo()
+//..gibt z. B. Folgendes auf der Konsole aus
 
-    //mangoNougat.getLengthOfDescription();
-    chocolate.getLengthOfDescription();
+vanilla.printInfo();
+mangoNougat.printInfo();
+
+//..getLengthOfDescription()
+//..gibt die Textlänge von description aus oder 0, wenn die Eigenschaft nicht gesetzt ist
+
+//mangoNougat.getLengthOfDescription();
+chocolate.getLengthOfDescription();
+
+//& BONUS DOM
+
+const iceSection = document.querySelector("#ice-cream");
 
 
+
+if (iceSection) {
+  iceSection.className = "grid grid-cols-4 gap-5";
+}
+
+iceCreamFlavors.forEach((flavor) => {
+    let scoopAmount: number = 0;
+
+  const iceDiv = document.createElement("div");
+  const popularDiv = document.createElement("div");
+  const iceHeadline = document.createElement("h1");
+  const icePrice = document.createElement("p");
+  const iceAmountScoops = document.createElement("p");
+  const icePriceTotal = document.createElement("p");
+  const iceButtonAdd = document.createElement("button");
+
+  iceSection?.appendChild(iceDiv);
+  iceDiv.appendChild(popularDiv);
+  iceDiv.appendChild(iceHeadline);
+  iceDiv.appendChild(icePrice);
+  iceDiv.appendChild(iceAmountScoops);
+  iceDiv.appendChild(icePriceTotal);
+  iceDiv.appendChild(iceButtonAdd);
+
+
+  popularDiv.textContent = "💖"
+  if (flavor.isPopular === false) {
+    popularDiv.className = "invisible"
+  } 
+  iceHeadline.textContent = flavor.name;
+  icePrice.textContent = flavor.price.toString();
+  iceAmountScoops.textContent = `Scoops: ${scoopAmount} `
+  icePriceTotal.textContent = `${scoopAmount * flavor.price}` 
+  iceButtonAdd.textContent = "+1";
+
+  iceDiv.className = "bg-red-200 text-center"
+  iceButtonAdd.className = "bg-red-300 py-1 px-5"
+
+  iceButtonAdd.addEventListener("click", ()=> {
+    scoopAmount++
+    iceAmountScoops.textContent = `Scoops: ${scoopAmount} `
+    icePriceTotal.textContent = `${scoopAmount * flavor.price}` 
+    console.log(scoopAmount);
+  })
+});
 
 
 
@@ -102,52 +147,44 @@ const classThree = new SchoolClass(3, "Class Three");
 
 console.log(classOne);
 
-classTwo.id = 4
-classOne.name = "Klasse Eins"
-classOne.endDate = "May"
+classTwo.id = 4;
+classOne.name = "Klasse Eins";
+classOne.endDate = "May";
 
 console.log(classOne);
-
-
-
 
 //& OOP-Class-TS-Level-2_1
 
 //..Wir wollen unsere Schulklassenverwaltung (SchoolClass) nun weiter ausbauen.
-//..Wir möchte die Schulklasse mit Leben füllen und brauchen dafür eine Klasse Person mit folgenden Attributen/Eigenschaften: 
+//..Wir möchte die Schulklasse mit Leben füllen und brauchen dafür eine Klasse Person mit folgenden Attributen/Eigenschaften:
 //..id
 //..firstName
 //..lastName
 //..birthday
 //..Überlege dir hierzu selbst die passenden Datentypen.
 
-//..Füge der Klasse SchoolClass ein weiteres Attribut/eine Eigenschaft hinzu, und zwar ein Array aus Personen. 
+//..Füge der Klasse SchoolClass ein weiteres Attribut/eine Eigenschaft hinzu, und zwar ein Array aus Personen.
 
 //..Teste deine Klassen und Methoden in der app.ts. Erstelle eine Schulklasse und füge mehrere Personen hinzu
 
 //..d) Lass dir alle Personen aus der Schulklasse ausgeben
 //..du kannst eine Schleife zur Erstellung von den Personen benutzen.
 
+for (let i = 0; i <= 10; i++) {
+  const person = new Person();
+  person.id = i;
+  person.firstName = `firstName ${i}`;
+  person.lastName = `lastName ${i}`;
+  person.birthday = `date ${i}`;
 
-for (let i = 0; i<= 10; i++) {
-    const person = new Person();
-    person.id = i;
-    person.firstName = `firstName ${i}`
-    person.lastName = `lastName ${i}`
-    person.birthday = `date ${i}`
+  //unteres ohne Methode!
+  //classOne.classMates.push(person)
 
-
-    //unteres ohne Methode!
-    //classOne.classMates.push(person)
-
-    //da ich classMates auf private gestellt habe, habe ich eine Methode erstellen müssen, um die classMates hinzuzufügen-->diese Methode rufe ich hier auf
-    classOne.addClassMate(person)
+  //da ich classMates auf private gestellt habe, habe ich eine Methode erstellen müssen, um die classMates hinzuzufügen-->diese Methode rufe ich hier auf
+  classOne.addClassMate(person);
 }
 
 console.log(classOne);
-
-
-
 
 //& OOP-Class-TS-Level-2_2
 
@@ -167,59 +204,51 @@ console.log(classOne);
 
 //..Zu guter Letzt sollst du in der Datei app.ts, mehrere Autos und Fahrer erstellen und miteinander verknüpfen
 
+const carOne = new Car("Seat Ibiza", 2005, 120, CarType.Hatchback);
+const carTwo = new Car("VW Golf", 1995, 199, CarType.Convertible);
+const carThree = new Car("VW T1", 1980, 70, CarType.Unknown);
 
-const carOne = new Car("Seat Ibiza", 2005, 120, CarType.Hatchback)
-const carTwo = new Car("VW Golf", 1995, 199, CarType.Convertible)
-const carThree = new Car("VW T1", 1980, 70, CarType.Unknown)
-
-
-const driverOne = new Driver("Yvi", "Lodi", 36, carOne, 2008)
+const driverOne = new Driver("Yvi", "Lodi", 36, carOne, 2008);
 console.log(driverOne);
-const driverTwo = new Driver("Bast", "K", 30, carTwo, 2014)
+const driverTwo = new Driver("Basti", "K", 30, carTwo, 2014);
 console.log(driverTwo);
-const driverThree = new Driver("Lotte", "", 30, carThree, 2014)
+const driverThree = new Driver("Lotte", "", 30, carThree, 2014);
 console.log(driverThree);
-
-
-
 
 //& OOP-Class-TS-Level-1_13
 
 //..Wir bauen auf den Klassen Car und Driver auf
 //..in der Klasse Car
-    //..soll constructionYear nur über den Konstruktor gesetzt werden können 
-    //..Wert ist nur über einen Getter (get-Methode) abrufbar
+//..soll constructionYear nur über den Konstruktor gesetzt werden können
+//..Wert ist nur über einen Getter (get-Methode) abrufbar
 
-    console.log(carOne.constructionYear);
-    console.log(carTwo.constructionYear);
-    console.log(carThree.constructionYear);
+console.log(carOne.constructionYear);
+console.log(carTwo.constructionYear);
+console.log(carThree.constructionYear);
 
-    //..soll es eine Methode getSpeedInfo(): string geben mit dieser Logik geben
-    //..currentSpeed ≤ 40 → Rückgabewert ‘Driving slowly’
-    //..currentSpeed > 40 und ≤ 70 → Rückgabewert ‘Driving normally’
-    //..currentSpeed > 70 und ≤ 120 → Rückgabewert ‘Driving fast’
-    //..currentSpeed > 120 und ≤ 190 → Rückgabewert ‘Driving really fast’
-    //..currentSpeed> 190 → ‘WTF’
+//..soll es eine Methode getSpeedInfo(): string geben mit dieser Logik geben
+//..currentSpeed ≤ 40 → Rückgabewert ‘Driving slowly’
+//..currentSpeed > 40 und ≤ 70 → Rückgabewert ‘Driving normally’
+//..currentSpeed > 70 und ≤ 120 → Rückgabewert ‘Driving fast’
+//..currentSpeed > 120 und ≤ 190 → Rückgabewert ‘Driving really fast’
+//..currentSpeed> 190 → ‘WTF’
 
-        console.log(carOne.getSpeedInfo());
-        console.log(carTwo.getSpeedInfo());
-        console.log(carThree.getSpeedInfo());
-
+console.log(carOne.getSpeedInfo());
+console.log(carTwo.getSpeedInfo());
+console.log(carThree.getSpeedInfo());
 
 //..Lege in einer neuen Datei ein Enum namens CarType an
-//..Werte → Unknown, Sedan, SUV, Coupe, Convertible, Hatchback, Truck   
+//..Werte → Unknown, Sedan, SUV, Coupe, Convertible, Hatchback, Truck
 //..erweitere die Klasse Car um eine neue Eigenschaft carType: CarType
 
 //..in der Klasse Driver
-    //..soll es eine weitere Eigenschaft licenseFromYear: number geben, die nur über den Konstruktor gesetzt werden kann
-    //..Wert ist nur über einen Getter (get-Methode) abrufbar
-    //..soll es eine Methode getCarType(): CarType geben, die den CarType des aktuellen Autos als Text ausgibt
+//..soll es eine weitere Eigenschaft licenseFromYear: number geben, die nur über den Konstruktor gesetzt werden kann
+//..Wert ist nur über einen Getter (get-Methode) abrufbar
+//..soll es eine Methode getCarType(): CarType geben, die den CarType des aktuellen Autos als Text ausgibt
 
-    console.log(driverOne.getCarType());
-    console.log(driverTwo.getCarType());
-    console.log(driverThree.getCarType());
-
-
+console.log(driverOne.getCarType());
+console.log(driverTwo.getCarType());
+console.log(driverThree.getCarType());
 
 //& OOP-Class-TS-Level-3_1
 
@@ -253,50 +282,53 @@ console.log(driverThree);
 
 //..Lege 3 Serien mit ein paar Episoden und Darsteller:innen an
 
-
-
-const actor1 = new Actor("Bryan", "Crabston", "1956-03-07", "Male")
-const actor2 = new Actor("Aaron", "Paul", "1979-08-27", "Male")
-const actor3 = new Actor("Anna", "Gunn", "1968-04-11", "Female")
+const actor1 = new Actor("Bryan", "Crabston", "1956-03-07", "Male");
+const actor2 = new Actor("Aaron", "Paul", "1979-08-27", "Male");
+const actor3 = new Actor("Anna", "Gunn", "1968-04-11", "Female");
 
 const allActorsEpisode1 = [];
-allActorsEpisode1.push(actor1)
-allActorsEpisode1.push(actor2)
-allActorsEpisode1.push(actor3)
+allActorsEpisode1.push(actor1);
+allActorsEpisode1.push(actor2);
+allActorsEpisode1.push(actor3);
 
 const allActorsEpisode2 = [];
-allActorsEpisode2.push(actor1)
-allActorsEpisode2.push(actor2)
+allActorsEpisode2.push(actor1);
+allActorsEpisode2.push(actor2);
 
-const episode1 = new Episode("Episode 1", "120 minutes", "", allActorsEpisode1)
-const episode2 = new Episode("Episode 2", "60 minutes", "", allActorsEpisode2)
+const episode1 = new Episode("Episode 1", "120 minutes", "", allActorsEpisode1);
+const episode2 = new Episode("Episode 2", "60 minutes", "", allActorsEpisode2);
 
+const allEpisodes = [];
+allEpisodes.push(episode1);
+allEpisodes.push(episode2);
 
-const allEpisodes = []
-allEpisodes.push(episode1)
-allEpisodes.push(episode2)
-
-const tvShow1 = new TVShow("Breaking Bad", "A high school chemistry teacher turned methamphetamine manufacturer", 2008, 2013, allEpisodes);
+const tvShow1 = new TVShow(
+  "Breaking Bad",
+  "A high school chemistry teacher turned methamphetamine manufacturer",
+  2008,
+  2013,
+  allEpisodes
+);
 
 //..Schreibe eine Funktion printSeriesInfo(series: Series), die beispielhaft für eine Serie Folgendes auf der Konsole ausgibt.
 
+function printSeriesInfo(series: TVShow) {
+  console.log(`${series.title}`);
+  console.log(series.description);
+  console.log(series.startYear);
+  console.log(series.endYear);
+  console.log(series.episodes.length);
 
-function printSeriesInfo (series: TVShow) {
-    console.log(`${series.title}`);
-    console.log(series.description);
-    console.log(series.startYear);
-    console.log(series.endYear);
-    console.log(series.episodes.length);
-
-
-    series.episodes.forEach((episode)=> {
-        episode.actors.forEach((actor)=> {
-            console.log(`${actor.firstName} ${actor.lastName}: -Birthday: ${actor.birthday}, -Gender: ${actor.gender}`);
-        })
+  series.episodes.forEach((episode) => {
+    episode.actors.forEach((actor) => {
+      console.log(
+        `${actor.firstName} ${actor.lastName}: -Birthday: ${actor.birthday}, -Gender: ${actor.gender}`
+      );
     });
+  });
 
-    //! Hier um Dopplung zu vermeiden, von Julia
-     // Array, der die Schauspieler nur einzeln aufführt
+  //! Hier um Dopplung zu vermeiden, von Julia
+  // Array, der die Schauspieler nur einzeln aufführt
   const allActors: Actor[] = [];
   // Dopplung von Schauspielern vermeiden
   series.episodes.flatMap((episode) =>
@@ -304,19 +336,15 @@ function printSeriesInfo (series: TVShow) {
       if (
         !allActors.some(
           (a) =>
-            a.firstName === actor.firstName && a.lastName === actor.lastName,
+            a.firstName === actor.firstName && a.lastName === actor.lastName
         )
       )
         allActors.push(actor);
-    }),
+    })
   );
   console.log(allActors);
 }
 
-printSeriesInfo(tvShow1)
-
+printSeriesInfo(tvShow1);
 
 //..Bei Actors sollen alle Darsteller:innen aus allen Episoden ausgegeben werden.
-
-
-
